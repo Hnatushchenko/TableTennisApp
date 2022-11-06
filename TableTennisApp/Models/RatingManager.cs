a0ff55ccc9c68beb5a612e351a7346f842bf6d2f
cvsxi <mrhappy795@gmail.com>
2022-11-06 17:48:41 +0200

    added rating

---
+++ TableTennisApp/Models/RatingManager.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp2;
+using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		static void RatingCount(Player PlayerWhoWon,Player PlayerWhoLost)
+		{
+			PlayerWhoWon.Rating += 30;
+			PlayerWhoLost.Rating -= 27;
+			
+		}
+
+	}
+}
