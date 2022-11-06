90fcafec1339da1a8ed79d732fb166eb30b75fee
cvsxi <mrhappy795@gmail.com>
2022-11-06 14:37:46 +0200

    'add'

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Player
+    {
+        public string Name { get; set; }
+        public int TotalGames { get; set; }
+        public Player(int totalGames)
+        {
+            TotalGames =totalGames;
+            
+        }
+        public Player(string name)
+        {
+            Name = name;
+          
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Queue
+    {
+        public List <Player> Players = new List<Player>();
+        public void AddPlayer(Player player)
+        {
+            
+            for (int i = 0; i < Players.Count; i++)
+            {
+                if (Players[i].Name == player.Name)
+                {
+                    return;
+                }
+         
+            }
+            Players.Add(player);
+        }
+        public void RemoveFirst()
+        {
+            Players.RemoveAt(0);
+            
+        }
+        public void RemovePlayer(Player player)
+        {
+            Player P=Players.First(p=>p.Name==player.Name);
+            Players.Remove(P);
+
+        }
+    }
+}
