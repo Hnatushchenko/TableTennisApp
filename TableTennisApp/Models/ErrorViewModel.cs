415e90b96fcde6fc92339b2a5d41595bb0879211
Hnatushchenko <gnatushenko.andrij@gmail.com>
2022-11-06 13:58:10 +0200

    Initial commit

---
+++ TableTennisApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TableTennisApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
