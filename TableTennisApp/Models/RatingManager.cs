29f6c1178e6e4e16311137e6654a80d16637f7fd
Hnatushchenko <gnatushenko.andrij@gmail.com>
2022-11-07 13:30:23 +0200

    Fix rating computing

--- TableTennisApp/Models/RatingManager.cs
+++ TableTennisApp/Models/RatingManager.cs
@@ -2,29 +2,12 @@
 {
     public class RatingManager
     {
-        public void CalculateNewRating(Player player1, Player player2, int scoreDifference)
+        public void CalculateNewRating(Player playerWhoWon, Player playerWhoLost)
         {
-            double K = scoreDifference switch
-            {
-                < 0 => -1,
-                0 => 0.5,
-                > 0 => 1
-            };
-            double difference = Math.Abs(player2.Rating - player1.Rating);
+            double difference = playerWhoLost.Rating - playerWhoWon.Rating;
             double expectedScore = 1 / (1 + Math.Pow(10, difference / 400));
-            double ratio = K switch
-            {
-                -1 => -1 + expectedScore,
-                0.5 => 0.5 - expectedScore,
-                1 => 1 - expectedScore,
-                _ => throw new Exception($"K cannot be equal to {K}")
-            };
-            if (scoreDifference == 0 && player1.Rating > player2.Rating)
-            {
-                (player1, player2) = (player2, player1);
-            }
-            player1.Rating += Convert.ToInt32(20 * ratio);
-            player2.Rating -= Convert.ToInt32(20 * ratio);
+            playerWhoWon.Rating += Convert.ToInt32(20 * (1 - expectedScore));
+            playerWhoLost.Rating -= Convert.ToInt32(20 * (1 - expectedScore));
         }
     }
 }
